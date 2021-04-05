b7817115491a9ef07ce1e6d8a644577347c38c45
Djkde <sergio.estrella@utp.edu.co>
2021-04-05 16:38:01 -0500

    [NEW] Component ProductsDashboard & mock data list

--- src/components/InitialComponent.jsx
+++ src/components/InitialComponent.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
 import "@styles/components/InitialComponent.css";
-import ProductCard from "./ProductCard/ProductCard";
+import ProductsDashboard from "./ProductsDashboard/ProductsDashboard";
 
 const InitialComponent = () => {
   return (
     <div className="initial-component">
       <h3>Your project is running</h3>
       <h1>Let's code!</h1>
-      <ProductCard />
+      <ProductsDashboard />
     </div>
   );
 };
---
+++ src/data/productList.js
@@ -0,0 +1,52 @@
+const productsList = [
+  {
+    id: 1,
+    productName: "Seif",
+    productImage:
+      "https://i.picsum.photos/id/834/151/151.jpg?hmac=vpknYQxyVDupPYDk1Tn0LplegEWI7Fnl-5Nzo34lCvQ",
+  },
+  {
+    id: 2,
+    productName: "Seif Módulos",
+    productImage:
+      "https://i.picsum.photos/id/834/151/151.jpg?hmac=vpknYQxyVDupPYDk1Tn0LplegEWI7Fnl-5Nzo34lCvQ",
+  },
+  {
+    id: 3,
+    productName: "Controller",
+    productImage:
+      "https://i.picsum.photos/id/834/151/151.jpg?hmac=vpknYQxyVDupPYDk1Tn0LplegEWI7Fnl-5Nzo34lCvQ",
+  },
+  {
+    id: 4,
+    productName: "Visitantes Robusto",
+    productImage:
+      "https://i.picsum.photos/id/834/151/151.jpg?hmac=vpknYQxyVDupPYDk1Tn0LplegEWI7Fnl-5Nzo34lCvQ",
+  },
+  {
+    id: 5,
+    productName: "PTT",
+    productImage:
+      "https://i.picsum.photos/id/834/151/151.jpg?hmac=vpknYQxyVDupPYDk1Tn0LplegEWI7Fnl-5Nzo34lCvQ",
+  },
+  {
+    id: 6,
+    productName: "Controlpharm",
+    productImage:
+      "https://i.picsum.photos/id/834/151/151.jpg?hmac=vpknYQxyVDupPYDk1Tn0LplegEWI7Fnl-5Nzo34lCvQ",
+  },
+  {
+    id: 7,
+    productName: "Mock Product",
+    productImage:
+      "https://i.picsum.photos/id/834/151/151.jpg?hmac=vpknYQxyVDupPYDk1Tn0LplegEWI7Fnl-5Nzo34lCvQ",
+  },
+  {
+    id: 8,
+    productName: "Another Mock",
+    productImage:
+      "https://i.picsum.photos/id/834/151/151.jpg?hmac=vpknYQxyVDupPYDk1Tn0LplegEWI7Fnl-5Nzo34lCvQ",
+  },
+];
+
+export default productsList;
